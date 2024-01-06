345c809f4bdb5456f3449e776742c8279bd0ac92
Mohammed7amada <m7amd7amada@gmail.com>
2024-01-06 21:58:36 +0200

    Update The Template

--- Api/Extensions/DependencyInjection.cs
+++ Api/Extensions/DependencyInjection.cs
@@ -1,5 +1,4 @@
 using DataAccess.Data;
-using DataAccess.Repositories;
 
 using Domain.Interfaces;
 
--- Api/Program.cs
+++ Api/Program.cs
@@ -4,11 +4,20 @@ var app = WebApplication.CreateBuilder().ConfigureServices().Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/error");
+}
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.MapControllers();
+app.MapGet("/errors", () => "Error!");
 
 await app.RunAsync();
